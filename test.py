cac2003a959c2688129461c65fff1abf3e6eddb3
bhill1025 <hill.bri@northeastern.edu>
2021-11-18 13:45:28 -0500

    Got the crawler to work. Just needs chunk handling and command line implementation

--- test.py
+++ test.py
@@ -2,14 +2,20 @@
 import socket
 import sys
 import ssl
+import time
 from html.parser import HTMLParser
 
-csrftoken = ""
-username = ""
-password = ""
-csrfmiddlewaretoken = ""
-nextt = ""
+import datetime
 
+csrftoken = ''
+
+# username = "jarrin.y"
+# password = "ZDPZ6OOA7MNCW512"
+
+username = "hill.bri"
+password = "QYHNOH9FA44LSQ12"
+
+nextt = "%2Ffakebook%2F"
 csrfmiddle = ''
 
 comp = ""
@@ -17,37 +23,13 @@ flags = set()
 comps = set()
 history = set()
 frontier = set()
+csrftoken = ""
+sessionid = ""
+cookie = ""
+crash_cnt = 0
 
-mock_links_3 = {
-    '1' : '<a href=\"2\">mock</a>',
-    '2' : '<a href=\"3\">mock</a>\r\n<a href=\"4\">mock</a>',
-    '3' : '<a href=\"5\">mock</a>\r\n<a href=\"1\">mock</a>\r\n<a href=\"2\">mock</a>\r\n<a href=\"8\">mock</a>',
-    '4' : '<a href=\"17\">mock</a>\r\n<a href=\"10\">mock</a>\r\n<a href=\"11\">mock</a>',
-    '5' : '<a href=\"3\">mock</a>\r\n<a href=\"4\">mock</a>',
-    '6' : '<a href=\"1\">mock</a>\r\n<a href=\"13\">mock</a>',
-    '7' : '<a href=\"3\">mock</a>\r\n<h2 class=\'secret_flag\' style=\"color:red\">FLAG: Got_7</h2>\r\n<a href=\"2\">mock</a>',
-    '8' : '<a href=\"12\">mock</a>\r\n<a href=\"9\">mock</a>',
-    '9' : '<a href=\"7\">mock</a>\r\n<a href=\"8\">mock</a>',
-    '10' : '<a href=\"9\">mock</a>\r\n<a href=\"10\">mock</a>',
-    '11' : '<a href=\"3\">mock</a>\r\n<a href=\"6\">mock</a>',
-    '12' : '<h2 class=\'secret_flag\' style=\"color:red\">FLAG: Got_12</h2>\r\n<a href=\"1\">mock</a>',
-    '13' : '<h2 class=\'secret_flag\' style=\"color:red\">FLAG: Got_13</h2>\r\n<a href=\"3\">mock</a>\r\n<a href=\"6\">mock</a>',
-    '14' : '<a href=\"20\">mock</a>\r\n<a href=\"15\">mock</a>',
-    '15' : '<a href=\"16\">mock</a>\r\n<a href=\"18\">mock</a>',
-    '16' : '<h2 class=\'secret_flag\' style=\"color:red\">FLAG: Got_16</h2>',
-    '17' : '<a href=\"14\">mock</a>\r\n<a href=\"16\">mock</a>',
-    '18' : '<a href=\"19\">mock</a>\r\n<a href=\"5\">mock</a>',
-    '19' : '<h2 class=\'secret_flag\' style=\"color:red\">FLAG: Got_19</h2>',
-    '20' : '<a href=\"1\">mock</a>\r\n<a href=\"2\">mock</a>',
-}
-
-mock_start_3 = '<a href=\"1\">mock</a>'
-
-mock_csrf = '<input type="hidden" name="csrfmiddlewaretoken" value="zeVpBxYQf2FA3XLPXBOXn6k03urJ2SXK3QW7SJ5IigjiVym9NSSXw9cupy0RdvGp">'
-
-class parserGET(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
+start = ''
+end = ''
 
 class MyHTMLParser(HTMLParser):
 
@@ -59,7 +41,7 @@ class MyHTMLParser(HTMLParser):
             for at in attrs:
                 comp += str(at) + " "
             if tag == 'a':
-                if attrs[0][1] not in history:
+                if attrs[0][1] not in history and '/fakebook/' in attrs[0][1]:
                     frontier.add(attrs[0][1])
         elif tag == 'input':
             if attrs[1][1] == 'csrfmiddlewaretoken':
@@ -80,61 +62,70 @@ class MyHTMLParser(HTMLParser):
             if "secret_flag" in comp:
                 flag = data
                 f_split = flag.split(": ")
+                # print(flag)
                 flags.add(f_split[1])
+                if len(flags) >= 5:
+                    end = datetime.datetime.now()
+                    for f in flags:
+                        print(f)
+
+                    # print(start,":", end)
+                    print(end - start)
+                    print("Crashed", crash_cnt, "times.")
+                    exit(0)
             comp += data
         # print("Encountered some data  :", data)
 
 # takes html element and crawls it, as well as links inside it
 def process_html(html):
+    global cookie
+
     parser = MyHTMLParser()
     parser.feed(html)
 
     #loop for next html
     while len(frontier) > 0:
+        
         link = frontier.pop()
+        history.add(link)
 
         # this will need to be replaced with our GET method.
         # need to return an html string from the response.
-        next_html = mock_links_3[link]
+        resp = get_request(link, cookie)
+        if('Transfer-Encoding: chunked' in resp):
+            t = ""
+        # print(link)
+        # print('**********************************')
+        # print(resp)
+        # print("----------------------------------")
+        if resp == "":
+            print("Something went wrong")
+            print(link)
+            print(flags)
+            sock
+            break
+        
+        next_header, next_html = process_response(resp)
+        try:
+            if next_header['status'] != '200':
+                print(next_header['status'])
+        except:
+            print(resp)
+        cookie = build_cookie()
+        parser.feed(next_html)
 
-        history.add(link)
-        process_html(next_html)
+        # process_html(next_html)
 
-
-#create a socket to the host on the https port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    s = ssl.wrap_socket(sock) 
-    s.connect(("fakebook.3700.network", 443))
-    s.getsockopt( socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-
-"""
-if( x == 0):
-            print 'Socket Keepalive off, turning on'
-            x = sock.setsockopt( socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-            print 'setsockopt=', x
-        else:
-            print 'Socket Keepalive already on'
-"""
-
-#do a GET request for the homepage
-# @@@@ fakebook.3700.network/
-request = "GET /accounts/login/?next=/fakebook/ HTTP/1.0\r\n\r\n"
-s.sendall(bytes(request, 'utf-8'))
-#s.send(request.encode()) #@@@@@ when do you implement keep alive??
-
-#recive the answer from the GET
-resp = s.recv(4096).decode('utf-8') 
-# print(response)
-
-#send POST request to login
-pRequest = "POST /accounts/login/ HTTP/1.0"
-parser = parserGET()
-# parser.feed(response)
-
+        
 def process_response(response):
+    global csrftoken
+    global sessionid
     split_response = response.split("\r\n\r\n\n\n\n")
     header = split_response[0]
-    html = split_response[1]
+    try:
+        html = split_response[1]
+    except:
+        html = ""
     split_header = header.split("\r\n")
 
     header_dict = {}
@@ -142,21 +133,135 @@ def process_response(response):
         if 'HTTP/1.1 ' in item:
             sp = item.split(' ')
             header_dict['status'] = sp[1]
-        else:
+        elif item != "":
             sp = item.split(': ')
+            if sp[0] == 'Set-Cookie':
+                if 'csfrtoken=' in sp[1]:
+                    csrftoken = sp[1]
+                elif 'sessionid=' in sp[1]:
+                    sessionid = sp[1]
             header_dict[sp[0]] = sp[1]
     return header_dict, html
 
+def build_cookie():
+
+    c1_split = csrftoken.split()
+    c2_split = sessionid.split()
+
+    cookie = c1_split[0] + '; ' + c2_split[0]
+    return cookie[:-1]
+
+
+def get_request(url, cookie):
+    global s, crash_cnt
+    resp = ""
+    cnt = 0
+    while resp == "" and not (cnt > 10):
+        if cnt > 1:
+            time.sleep(5)
+        request = f"GET {url} HTTP/1.1\r\nConnection: keep-alive\r\nCookie: {cookie}\r\nHost: fakebook.5700.network\r\n\r\n"
+        # print(request)
+        try:
+            s.sendall(bytes(request, 'utf-8'))
+        except:
+            crash_cnt += 1
+            s.close()
+            # print("closed")
+            time.sleep(10)
+            # print("trying to reopen")
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                s = ssl.wrap_socket(sock) 
+                s.connect(("fakebook.3700.network", 443))
+                s.getsockopt( socket.SOL_SOCKET, socket.SO_KEEPALIVE, 30)
+            try:
+                s.sendall(bytes(request, 'utf-8'))
+                # print("success!")
+            except:
+                print("Broke Twice")
+                exit(1)
+
+
+        # recieve the response
+        resp = s.recv(4096).decode('utf-8')
+        cnt += 1
+
+    if cnt > 10:
+        print("Aborting")
+        print(request)
+    return resp
+        
+#create a socket to the host on the https port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    s = ssl.wrap_socket(sock) 
+    s.connect(("fakebook.3700.network", 443))
+    s.getsockopt( socket.SOL_SOCKET, socket.SO_KEEPALIVE, 30)
+
+start = datetime.datetime.now()
+
+#do a GET request for the homepage
+# @@@@ fakebook.3700.network/
+request = "GET /accounts/login/?next=/fakebook/ HTTP/1.1\r\nConnection: keep-alive\r\nHost: fakebook.5700.network\r\n\r\n"
+s.sendall(bytes(request, 'utf-8'))
+
+#recive the answer from the GET
+resp = s.recv(4096).decode('utf-8') 
+# print(resp)
+
+
 my_header, my_html = process_response(resp)
 
-cookie = my_header['Set-Cookie'].split()[0][:-1]
+try:
+    csrftoken = my_header['Set-Cookie'].split()[0][:-1]
+except:
+    print(my_header)
+    print(resp)
 
 parser = MyHTMLParser()
 parser.feed(my_html)
 
-print(cookie)
+# print("the csrf token we found: " + str(csrftoken))
+# print("the csrfmiddle token we found" + str(csrfmiddle))
+
+postStuff = "Host: fakebook.3700.network\r\n" + "Connection: keep-alive\r\n" + "Content-Length: 148\r\n" + "Cache-Control: max-age=0\r\n"
+postStuff += "sec-ch-ua: \"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"\r\n"
+postStuff += "sec-ch-ua-mobile: ?0\r\n" + "sec-ch-ua-platform: \"Windows\"\r\n" + "Upgrade-Insecure-Requests: 1\r\n"
+postStuff += "Origin: https://fakebook.3700.network\r\n" + "Content-Type: application/x-www-form-urlencoded\r\n"
+postStuff += "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36\r\n"
+postStuff += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n"
+postStuff += "cp-extension-installed: Yes\r\n" + "Sec-Fetch-Site: same-origin\r\n" + "Sec-Fetch-Mode: navigate\r\n" + "Sec-Fetch-User: ?1\r\n"
+postStuff += "Sec-Fetch-Dest: document\r\n" + "Referer: https://fakebook.3700.network/accounts/login/?next=/fakebook/\r\n"
+postStuff += "Accept-Encoding: gzip, deflate, br\r\n" + "Accept-Language: en-US,en;q=0.9,fr;q=0.8\r\n"
 
 
+cookieText = "Cookie: " + csrftoken + "\r\n\r\n"
+# added the \r\n\r\n becuase I think what comes next is the body
+userPassToken = "username=" + username + "&password=" + password + "&csrfmiddlewaretoken=" + csrfmiddle + "&next=" + nextt + "\r\n\r\n"
 
-# print(csrfmiddle)
+#send POST request to login
+pRequest = "POST /accounts/login/ HTTP/1.0\r\n"
+pRequest += postStuff
+pRequest += cookieText
+pRequest += userPassToken
+
+# print("pRequest :" + pRequest)
+
+#send the POST to login
+s.sendall(bytes(pRequest, 'utf-8'))
+
+#what received after loggin in
+resp = s.recv(4096).decode('utf-8')
+# print("what received after login in" + resp)
+
+my_header, my_html = process_response(resp)
+
+cookie = build_cookie()
+
+url = '/fakebook/'
+
+resp = get_request(url, cookie)
+
+my_header, my_html = process_response(resp)
+cookie = build_cookie()
+process_html(my_html)
+
 # print(frontier)
